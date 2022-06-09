5deacc5b6e80a0cce989e845fb8f3e9adf397ae4
alice <tangch55@outlook.com>
2022-06-09 12:58:21 +0800

    ADD TRAINING MODULE FOR RELU NETWORKS
    * add pytorch based training module of relu networks

---
+++ ReLU_networks.py
@@ -0,0 +1,84 @@
+# pytorch implemetation
+import numpy as np
+import torch
+import torchvision.datasets as dts
+import torchvision.transforms as trnsfrms
+import torch.nn as nn
+
+
+class classicationmodel(nn.Module):
+    def __init__(self, network_params=(24, 24, 10), d_in=28*28):
+        super(classicationmodel,self).__init__()
+        self.linears = []
+        self.d_in = d_in
+        for i, d_out in enumerate(network_params):
+            lin_layer = nn.Linear(d_in, d_out)
+            self.linears.append(lin_layer)
+            self.add_module("linear_{}".format(i), lin_layer)
+            d_in = d_out
+        self.relu = nn.ReLU()
+
+    def forward(self, image):
+        a = image.view(-1, self.d_in)
+        for linear in self.linears:
+            a = self.relu(linear(a))
+        return a
+
+
+def train(cmodel, train_dataloader, d_in=28*28, criter='CE', l_r=0.001, numepchs=5):
+    loss = None
+    if criter == 'CE':
+        loss = nn.CrossEntropyLoss()
+    optim = torch.optim.Adam(cmodel.parameters(), lr=l_r)
+    nttlstps = len(train_dataloader)
+    #lr_scheduler = torch.optim.lr_scheduler.StepLR(optim, step_size=5, gamma=2)
+
+    torch.device('cpu')
+    for epoch in range(numepchs):
+        for i, (imgs, lbls) in enumerate(train_dataloader):
+            #lr_scheduler.step()
+            imgs = imgs.reshape(-1, d_in)
+
+            outp = cmodel(imgs)
+            losses = loss(outp, lbls)
+
+            optim.zero_grad()
+            losses.backward()
+            optim.step()
+
+            if (i + 1) % 100 == 0:
+                print('Epochs [{}/{}], Step[{}/{}], Losses: {}'.format(epoch + 1, numepchs, i + 1,
+                                                                       nttlstps, losses.item()))
+
+
+def test(cmodel, test_dataloader):
+    c_err = 0
+    n_data = 0
+    for imgs, lbls in test_dataloader:
+        outp = cmodel(imgs)
+        preds = np.argmax(outp.detach(), axis=1)
+        for p, l in zip(preds, lbls):
+            c_err += int(p!=l)
+            n_data += 1
+
+    print("Test acc: ", 1-c_err / n_data)
+
+
+if __name__  == "__main__":
+    cmodel = classicationmodel()
+
+    print(cmodel)
+    print(cmodel.parameters())
+
+    trnsform = trnsfrms.Compose([trnsfrms.ToTensor(), trnsfrms.Normalize((0.7,), (0.7,))])
+
+    mnisttrainset = dts.MNIST(root='./data', train=True, download=True, transform=trnsform)
+    trainldr = torch.utils.data.DataLoader(mnisttrainset, batch_size=10, shuffle=True)
+
+    mnist_testset = dts.MNIST(root='./data', train=False, download=True, transform=trnsform)
+    testldr = torch.utils.data.DataLoader(mnist_testset, batch_size=10, shuffle=True)
+
+    train(cmodel, trainldr, d_in=28*28, criter='CE', l_r=0.001, numepchs=5)
+
+    test(cmodel, testldr)
+
